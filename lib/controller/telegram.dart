8de70a3f40615b4a2897cd4935b9668d4b8d79f0
Rey33 <6danisimo6@gmail.com>
2024-09-30 00:52:36 +0300

    first commit

---
+++ lib/controller/telegram.dart
@@ -0,0 +1,47 @@
+import  'dart:convert' ;
+import  'package:flutter/widgets.dart' ;
+import  "package:get/get.dart" ;
+import  'dart:js'  as js;
+
+class  TelegramController  extends  GetxController  {
+  Map < String , dynamic >? telegramData;
+
+  @override
+  void onInit() {
+    super.onInit();
+    getTelegramData();
+  }
+
+  void getTelegramData() {
+    telegramData = initTelegramWebApp();
+    if (telegramData != null ) {
+      debugPrint( 'Telegram Data: $telegramData ' );
+    } else {
+      debugPrint( 'Telegram data is null. Это приложение открыто вне telegram' );
+    }
+    update();
+  }
+
+  // Функция для инициализации Telegram WebApp
+  static  Map < String , dynamic >? initTelegramWebApp() {
+    final result = js.context.callMethod( 'initTelegramWebApp' );
+    debugPrint( "result: $result " );
+    if (result != null ) {
+      // Преобразуем JsObject в строку JSON, а затем преобразуем ее в строку Map
+      var jsonString = js.context[ 'JSON' ].callMethod( 'stringify' , [result]);
+      return jsonDecode(jsonString);
+    }
+
+    return  null ;
+  }
+
+  // Функция для отправки данных обратно в Telegram
+  static  void sendTelegramData( String data) {
+    js.context.callMethod( 'sendTelegramData' , [data]);
+  }
+
+  // Функция для управления MainButton в Telegram
+  static  void setMainButton( String text, bool isVisible) {
+    js.context.callMethod( 'setMainButton' , [text, isVisible]);
+  }
+}
